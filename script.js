40d42c202a01529a3214e4b3fb3d411f9f01ab90
DinFloat <134521952+DinFloat@users.noreply.github.com>
2023-11-10 12:09:47 -0300

    configuração

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function mudarCoisas() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
 
+var a = window.document.getElementById('b1')
 function clicar1() {
     alert('One Piece é muito bom!!!')
 }
