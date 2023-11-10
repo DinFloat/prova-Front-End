843779c210f751fb77eb047b0af7c21abca0826c
DinFloat <134521952+DinFloat@users.noreply.github.com>
2023-11-10 12:08:22 -0300

    ...

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ function mudarCoisas() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
 
-function mudarCoisas() {
-    document.getElementById("demo2").innerHTML = "Luffy é muito very nice!";
-}
-
-function mudar() {
-    document.getElementById("imgDemo").innerHTML =  ;
+function clicar1() {
+    alert('One Piece é muito bom!!!')
 }
