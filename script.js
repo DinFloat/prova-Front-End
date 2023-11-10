4f5cb68936c3dc3ca0307229416ef341a2b33fdb
DinFloat <134521952+DinFloat@users.noreply.github.com>
2023-11-10 08:50:16 -0300

    adicoonando html e java novo

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
+
+function mudarTexto() {
+    document.getElementById("demo2").innerHTML = "Luffy é muito very nice!";
+}
